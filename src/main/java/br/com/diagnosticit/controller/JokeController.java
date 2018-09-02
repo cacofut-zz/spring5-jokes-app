65040a51b3ed201e4b57e2a732250f394a0e0f59
Cristiano <cacofut@hotmail.com>
2018-09-02 14:03:01 -0300

    Adionado chucknorris.html

--- src/main/java/br/com/diagnosticit/controller/JokeController.java
+++ src/main/java/br/com/diagnosticit/controller/JokeController.java
@@ -27,7 +27,7 @@ public class JokeController {
 
     @RequestMapping({"/", ""})
     public String showJoke( Model model ){
-        model.addAttribute( "", jokeService.getJoke() );
+        model.addAttribute( "joke", jokeService.getJoke() );
         return "chucknorris";
     }
     
